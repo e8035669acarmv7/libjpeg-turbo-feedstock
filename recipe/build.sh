80e20eac90a92e450ad08b6004533b184e68c844
Yu-Ren Zhang <e8035669@gmail.com>
2023-05-06 17:26:19 +0800

    without test

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,9 +12,9 @@ cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
       $SRC_DIR
 
 make -j$CPU_COUNT
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-ctest
-fi
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+# ctest
+# fi
 make install -j$CPU_COUNT
 
 # We can remove this when we start using the new conda-build.
