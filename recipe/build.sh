99e70bc3bf1189664e3daa0ffc2246aa4d317d0a
Yu-Ren Zhang <e8035669@gmail.com>
2024-07-13 11:13:02 +0800

    finally pass all test

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,14 @@ cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D ENABLE_SHARED=1 \
       -D WITH_JPEG8=1 \
       -D CMAKE_ASM_NASM_COMPILER=yasm \
+      -DFLOATTEST8=fp-contract \
+      -DFLOATTEST12=fp-contract \
       $SRC_DIR
 
 make -j$CPU_COUNT
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-# ctest
-# fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+ctest
+fi
 make install -j$CPU_COUNT
 
 # We can remove this when we start using the new conda-build.
